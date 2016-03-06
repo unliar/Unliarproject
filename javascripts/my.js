3a65eb3018ed181c5ef1b311692cc8039e5a0420
lzt224 <370732889@qq.com>
2016-03-06 12:20:07 +0800

    add get offsetwidth of client

--- javascripts/my.js
+++ javascripts/my.js
@@ -1,3 +1,12 @@
-window.onload=function bt(){
-	alert("这是远浅的Github练习项")
+window.onload = function bt() {
+    var TargetNode=document.getElementsByTagName("p")[1];
+	var pages = document.createElement("p");
+
+	function GetWidth() {
+		var scWidth = document.body.offsetWidth;
+		pages.innerText = "你的设备可视区域为: "+scWidth+"px";
+
+	}
+	setInterval(GetWidth, 200)
+TargetNode.insertBefore(pages,TargetNode.childNodes[0])
 }
